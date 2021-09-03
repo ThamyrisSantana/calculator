e8f8333a3811440f405447363a463610e1d8aca7
Thamyris <thamyrissatana3@gmail.com>
2021-09-03 14:28:30 -0300

    symbol added on screen

--- src/components/calculator screen/Screen.js
+++ src/components/calculator screen/Screen.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const screen = ({ screenNumber, screenNumber2, result, simbol }) => {
+const screen = ({ screenNumber, screenNumber2, result, symbol }) => {
   return (
     <div>
       <div>
         <span>{screenNumber}</span>
-        <span>{simbol}</span>
+        <span>{symbol}</span>
         <span>{screenNumber2}</span>
         <span>=</span>
         <span>{result}</span>
