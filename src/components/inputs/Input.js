b55f81105f923ae802c4141c3fc8d1a6ffb9b78a
Thamyris <thamyrissatana3@gmail.com>
2021-09-05 12:54:31 -0300

    change input to controlled

--- src/components/inputs/Input.js
+++ src/components/inputs/Input.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./input.scss";
 
-function Input({ setInputNumber }) {
+function Input({ value, setInputNumber }) {
   function getNumber(e) {
     setInputNumber(e.target.value);
   }
 
   return (
     <div>
-      <input type="number" onChange={getNumber} />
+      <input type="number" value={value} onChange={getNumber} />
     </div>
   );
 }
